e41f670ee3efefd10025582a0b4e9f5fe35ad5d3
jsrimr <jsrimr@naver.com>
2019-01-17 15:43:45 +0900

    endeavor_distribution_visual

--- env_agent.py
+++ env_agent.py
@@ -10,6 +10,7 @@ import numpy as np
 #from scipy import stats
 import random
 from tensorboardX import SummaryWriter
+from collections import Counter
 
 parser=argparse
 reward_pool=100000
@@ -24,11 +25,12 @@ tiny_value=0.05
 total_asset=100000
 explore_rate=0.3
 cost=10000
+range_endeavor=10
 
 
 class Env:
     def __init__(self):
-        self.action_space = np.arange(0,10)
+        self.action_space = np.arange(0,range_endeavor)
         self.n_actions = len(self.action_space)
         self.timestep = 0
         self.total_like=0
@@ -52,14 +54,14 @@ class Env:
             if agent.action>=1:
                 agent.review_history+=1
                 n_reviewers+=1
-            agent.receive_token(rewards[idx])
+#            agent.receive_token(rewards[idx])
             agent.learn(actions[idx],rewards[idx],cost)
         
         self.total_like=0
         self.timestep += 1
         
         review_ratio= n_reviewers/len(agents)
-        return review_ratio
+        return review_ratio,actions
     
     def get_reward(self,agent,total_like):        
         mu= agent.my_like/(self.total_like) * reward_pool
@@ -74,12 +76,6 @@ class Env:
 
 def distribute_asset(agents):
     n_agent=len(agents)    
-#    random_numbers = np.random.random(n_agent)
-#    # random samples of Rayleigh Dist. through the inverse transform
-#    random_numbers_from_rayleigh =np.sqrt(-2*np.log(1-random_numbers)) #[inv_rayleigh_cdf(number) for number in random_numbers]
-#    proportion=random_numbers_from_rayleigh/sum(random_numbers_from_rayleigh) #[num/sum(random_numbers_from_rayleigh) for num in random_numbers_from_rayleigh]
-#    
-    
     #np.random.pareto 분포를 쓰면 좋을듯
     probs=np.random.pareto(2,n_agent)
     for i in range(n_agent):
@@ -90,12 +86,14 @@ def distribute_asset(agents):
 from agent import Agent
 
 endeavor_list=[]
+final_list=[{} for _ in range(range_endeavor)]
+
 def run(env):
     global explore_rate
-    for episode in range(n_episode):
+    for episode in range(n_episode+1):
         print("episode {} starts".format(episode))
         distribute_asset(agents)
-        review_ratio=env.step(agents)
+        review_ratio,actions=env.step(agents)
         
         endeavor_list.append([agent.get_action(explore_rate=0) for agent in agents])
         explore_rate*=0.9
@@ -103,17 +101,25 @@ def run(env):
         #visualisation
         writer.add_scalar("data/review_ratio",review_ratio,episode)
         
-        
         for idx,agent in enumerate(agents):
             data_beta_table=dict( (str(i),v) for i,v in enumerate(agent.beta_table ))
             writer.add_scalars("data/{}".format(idx),data_beta_table,episode)
-        
+            
+        if episode%100==0:
+            counter=Counter(actions)
+            for act in range(range_endeavor): 
+                final_list[act][str(episode)]=counter[act]
+    print(final_list)
+            
+    for i in range(range_endeavor):
+        writer.add_scalars("data/endeavor",final_list[i],i)
 
 if __name__ == '__main__':
     writer=SummaryWriter()
     env = Env()
     agents = [Agent(action_space=env.action_space) for i in range(n_people)]
     run(env)
+    print(endeavor_list)
     writer.close()
         
 
