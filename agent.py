b39f0fe2a011c2dd83ef13a90689ebf70ce7b923
jsrimr <jsrimr@naver.com>
2019-01-18 14:10:40 +0900

    1. review_history deque 로 수정 +  review 쓸때만  history 에 1  append 하던거 안써도 0 append 하도록 변경 2. get_reward -> get_return 으로 이름 변경

--- agent.py
+++ agent.py
@@ -5,14 +5,15 @@ Created on Wed Jan 16 11:47:27 2019
 """
 import numpy as np
 import random
-
+from collections import deque
 
 class Agent:
     def __init__(self, action_space, args):
         self.endeavor = action_space  # action_space
         self.action = 0
         self.my_like = 0
-        self.review_history = [0 for _ in range(args.window)]  # 1 -> write 0 -> no review
+          # 1 -> write 0 -> no review
+        self.review_history = deque(maxlen=args.window)
         self.cost = args.cost
         self.asset_coef = args.asset_coef
         self.learning_rate = args.lr
@@ -20,7 +21,7 @@ class Agent:
         self.q_table = np.zeros_like(self.endeavor)
         self.beta_table = self.softmax(self.q_table)
         self.mu_table = [10 + 1 * i for i in self.endeavor]
-
+#[10,11,12...19]
         self.asset = 0  # 분배방식을 변경하며 분배해보자
         self.beta = args.beta
         self.alpha = args.alpha
@@ -29,6 +30,7 @@ class Agent:
         self.std_dev = args.std_dev
 
     def get_my_like(self):
+        # 노력과 review_history 의 합
         score = sum(self.review_history)
         if sum(self.review_history) == 0:
             score = len(self.review_history) + 1
@@ -65,10 +67,11 @@ class Agent:
         return cost
 
     def receive_token(self, amount_token):
+
         self.asset += amount_token
 
     def learn(self, action, reward):
         q1 = self.q_table[action]
         q2 = reward
-        self.q_table[action] += self.learning_rate * (q2 - q1) / self.beta_table[action]
+        self.q_table[action] += self.learning_rate * (q2 - q1) / self.beta_table[action] #확률이 적었던 선택은 크게 없데이트 할 수 있도록
         self.beta_table = self.softmax(self.q_table)
--- env.py
+++ env.py
@@ -7,12 +7,9 @@ class Env:
         self.action_space = np.arange(0., args.range_endeavor)
         self.n_actions = len(self.action_space)
         self.total_like = 0
-
         self.n_agent = args.n_agent
-        
         self.reward_pool = args.reward_pool
         
-
     def step(self, agents):
         actions = [agent.get_action() for agent in agents]
 
@@ -20,7 +17,7 @@ class Env:
             agent.my_like = agent.get_my_like()
             self.total_like += agent.my_like
 
-        returns = [float(self.get_reward(agent, self.total_like)) for agent in agents]
+        returns = [float(self.get_return(agent, self.total_like)) for agent in agents]
         costs = [float(agent.get_cost(actions[i])) for i, agent in enumerate(agents)]
         rewards = [ret - cost for ret, cost in zip(returns, costs)]
 #        print("returns : ",returns)
@@ -28,9 +25,10 @@ class Env:
         n_reviewers = 0
         for idx, agent in enumerate(agents):
             if agent.action >= 1:         
-                agent.review_history.pop(0)
                 agent.review_history.append(1)
                 n_reviewers += 1
+            else:
+                agent.review_history.append(0)
                 
             agent.learn(actions[idx], rewards[idx])
 
@@ -39,7 +37,7 @@ class Env:
         review_ratio = n_reviewers/len(agents)
         return review_ratio, actions
 
-    def get_reward(self, agent, total_like):
+    def get_return(self, agent, total_like):
         reward = agent.my_like/total_like* self.reward_pool
         return reward
 
