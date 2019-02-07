c75865f2cdc5e85523d7399d41c60ffa1c49f125
Luke Park <twodude@naver.com>
2019-02-07 14:40:24 +0900

    Update runpy.sh

--- runpy.sh
+++ runpy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 python3 run.py --mechanism="proportional" --n_agent=100 --reward_pool=500 --review_history=0 --window=5
 python3 run.py --mechanism="exponential" --n_agent=100 --reward_pool=500 --review_history=0 --window=5
 python3 run.py --mechanism="uniform" --n_agent=100 --reward_pool=500 --review_history=0 --window=5
